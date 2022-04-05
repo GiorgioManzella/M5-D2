6ebfc4e1a5ff2f14721cc66075ce45080c8cb396
giorgio <giorgiolink94@hotmail.it>
2022-04-05 22:49:34 +0100

    Update
    functional response from get

--- src/authors/index.js
+++ src/authors/index.js
@@ -1,19 +1,37 @@
 import express from "express";
-
+import fs from "fs"
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
 
 const authorsRouter = express.Router()
+const userJSONPath = ""
 
 
+const currentFilePath = fileURLToPath(import.meta.url)
+const parentFolder = dirname(currentFilePath)
+const authorsJsonPath = join(parentFolder, "authors.json")
 
-authorsRouter.post("/", () => {})
 
-authorsRouter.get("/", () => {})
+authorsRouter.post("/", (req, res) => {
 
-authorsRouter.get("/:authorsId", () => {})
+    res.send({message: "hello post is here"})
+    console.log('post funziona')
+})
 
-authorsRouter.put("/:authorsId", () => {})
+authorsRouter.get("/", (req, res) => {
+    const fileContent = fs.readFileSync(authorsJsonPath)
 
-authorsRouter.delete("/:authorsId", () => {})
+    const authorsArray = JSON.parse(fileContent)
+
+
+    res.send(authorsArray)
+})
+
+authorsRouter.get("/:authorsId", (req, res) => {})
+
+authorsRouter.put("/:authorsId", (req, res) => {})
+
+authorsRouter.delete("/:authorsId", (req, res) => {})
 
 
 export default authorsRouter
